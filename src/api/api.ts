971331ec71a7717f7be794b2488b66b17719972c
Maion Digital <developer@maiondigital.com>
2025-09-26 11:44:15 -0300

    Feat: API Router Class

---
+++ src/api/api.ts
@@ -0,0 +1,122 @@
+import { Toast } from "@/components/Toast/Toast"
+import axios, { AxiosError, AxiosResponse } from "axios"
+
+type TPrefixes =
+    "/allocation" |
+    "history"
+
+type TApiParams = {
+    prefix: TPrefixes
+    url: string
+    data?: object
+}
+
+type TAPIError = {
+    message: string
+    statusCode: number
+}
+
+class API {
+    static async POST(params: TApiParams): Promise<AxiosResponse<any, any, {}> | undefined> {
+        try {
+            const response = await axios.post(
+                `${process.env.NEXT_PUBLIC_API_URL}${params.prefix}${params.url}`,
+                params.data,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    withCredentials: true
+                }
+            )
+            return response
+        } catch(error) {
+            this.HandleError(error as AxiosError)
+        }
+    }
+
+    static async GET(params: TApiParams): Promise<AxiosResponse<any, any, {}> | undefined> {
+        try {
+            const response = await axios.get(
+                `${process.env.NEXT_PUBLIC_API_URL}${params.prefix}${params.url}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    withCredentials: true
+                }
+            )
+            return response
+        } catch(error) {
+            this.HandleError(error as AxiosError)
+        }
+    }
+
+    static async PUT(params: TApiParams): Promise<AxiosResponse<any, any, {}> | undefined> {
+        try {
+            const response = await axios.put(
+                `${process.env.NEXT_PUBLIC_API_URL}${params.prefix}${params.url}`,
+                params.data,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    withCredentials: true
+                }
+            )
+            return response
+        } catch(error) {
+            this.HandleError(error as AxiosError)
+        }
+    }
+
+    static async PATCH(params: TApiParams): Promise<AxiosResponse<any, any, {}> | undefined> {
+        try {
+            const response = await axios.patch(
+                `${process.env.NEXT_PUBLIC_API_URL}${params.prefix}${params.url}`,
+                params.data,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    withCredentials: true
+                }
+            )
+            return response
+        } catch(error) {
+            this.HandleError(error as AxiosError)
+        }
+    }
+
+    static async DELETE(params: TApiParams): Promise<AxiosResponse<any, any, {}> | undefined> {
+        try {
+            const response = await axios.delete(
+                `${process.env.NEXT_PUBLIC_API_URL}${params.prefix}${params.url}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    withCredentials: true
+                }
+            )
+            return response
+        } catch(error) {
+            this.HandleError(error as AxiosError)
+        }
+    }
+
+    static HandleSuccess() {
+        Toast.success("Operação realizada com sucesso")
+    }
+
+    static HandleError(error: AxiosError) {
+        const errorData = error.response?.data as TAPIError
+        
+        if (errorData?.message) {
+            Toast.error(errorData.message)
+        } else {
+            Toast.error("Ocorreu um erro ao realizar a operação")
+        }
+
+    }
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import { ToastContainer } from "react-toastify"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { Sidebar } from "@/components/Sidebar/Sidebar"
+import { QueryProvider } from "@/providers/QueryClientProvider"
 
 export default function RootLayout({
   children,
@@ -13,15 +14,17 @@ export default function RootLayout({
       <body
         className={`antialiased !overflow-x-hidden bg-black`}
       >
-        <SidebarProvider>
-          <Sidebar />
-          
-          <ToastContainer />
-            <main>
-              <SidebarTrigger className="bg-white cursor-pointer ms-2 mt-1" />
-              {children}
-            </main>
-        </SidebarProvider>
+        <QueryProvider>
+          <SidebarProvider>
+            <Sidebar />
+            
+            <ToastContainer />
+              <main>
+                <SidebarTrigger className="bg-white cursor-pointer ms-2 mt-1" />
+                {children}
+              </main>
+          </SidebarProvider>
+        </QueryProvider>
       </body>
     </html>
   );
